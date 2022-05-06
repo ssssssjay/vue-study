1b0970d4130f125211ef8370daeaa405e6f0149d
Sungjae.Kwak <sungjae8970@gmail.com>
2022-05-06 16:43:06 +0900

    responsecheck fin

--- 4.ResponseCheck/ResponseCheck.vue
+++ 4.ResponseCheck/ResponseCheck.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
+    <h1>반응속도체크게임</h1>
     <div id="screen" v-bind:class="state" @click="onClickScreen">{{ message }}</div>
-    <div>
-      <div>Response Time : {{ }}</div>
-      <button>눌러!!!</button>
+    <div v-show="result.length">
+      <!-- <div>Response Time Average : {{ result.reduce((a, c) => a + c , 0) / result.length || 0 }}ms</div> -->
+      <div>Response Time Average : {{ average }}ms</div>
+      <button @click="onReset">기록초기화</button>
     </div>
   </div>
 </template>
 
 <script>
+  let startTime = 0;
+  let endTime = 0;
+  let timeout = null;
   export default {
     data() {
       return {
@@ -17,18 +22,34 @@
         message : '클릭해서 시작하세요'
       }
     },
+    computed: {
+      average() {
+        return this.result.reduce((a, c) => a + c , 0) / this.result.length || 0
+      }
+    },
     methods: {
-      onResset() {
-
+      onReset() {
+        this.result = []
       },
       onClickScreen() {
         console.log('test');
         if (this.state === 'waiting') {
           this.state = 'ready';
+          this.message = '초록색으로 스크린이 변하면 클릭하세요!';
+          timeout = setTimeout(() => {
+            startTime = new Date();
+            this.state = 'now';
+            this.message = '지금입니다. 클릭하세요!'
+          }, Math.floor(Math.random() * 1000) + 2000);
         } else if (this.state === 'ready') {
-          this.state = 'now';
+          clearTimeout(timeout)
+          this.state = 'waiting';
+          this.message = '너무 급하게 클릭했습니다.'
         } else if (this.state === 'now') {
-          this.state = 'waiting'
+          endTime = new Date();
+          this.state = 'waiting';
+          this.message = '클릭해서 시작하세요';
+          this.result.push(endTime - startTime);
         }
       }
     }
