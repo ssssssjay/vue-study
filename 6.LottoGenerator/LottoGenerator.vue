abcbc06ecaa5205f4f1d916b275fa47e835d02ff
Sungjae.Kwak <sungjae8970@gmail.com>
2022-05-11 10:54:05 +0900

    7. 로또생성기 초기 세팅

---
+++ 6.LottoGenerator/LottoGenerator.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>test</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+  };
+</script>
+
+<style sccoped>
+
+</style>
