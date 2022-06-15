4061e751445c9140ec873ed1691cf393bc40077e
Sungjae.Kwak <sungjae8970@gmail.com>
2022-06-15 22:32:53 +0900

    vue multiple component & eventbus

---
+++ 7.TicTacToe/TableComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <table>
+    <tr-component 
+      v-for="(rowData, index) in tableData" :key="index" 
+      :row-data="rowData" 
+      :row-index="index">
+    </tr-component>
+  </table>
+</template>
+
+<script>
+  import TrComponent from './TrComponent';
+  export default {
+    name: 'TableComponent',
+    components: {
+      TrComponent,
+    },
+    props: {
+      tableData: Array,
+    },
+    computed: {
+    },
+  };
+</script>
+
+<style>
+  table {
+    border-collapse: collapse;
+  }
+  td {
+    border: 3px solid dodgerblue;
+    width: 40px;
+    height: 40px;
+    text-align: center;
+  }
+</style>
---
+++ 7.TicTacToe/TicTacToe.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div>{{turn}}님의 턴입니다</div>
+    <table-component :table-data="tableData" />
+    <div v-if="winner">{{winner}}님의 승리입니다.</div>
+  </div>
+</template>
+
+<script>
+  import TableComponent from './TableComponent';
+  export default {
+    components: {
+      // 'table-component': TableComponent,
+      TableComponent,
+    },
+    data() {
+      return {
+        tableData: [
+          ['', '', ''],
+          ['', '', ''],
+          ['', '', ''],
+        ],
+        turn: 'O',
+        winner: '',
+      }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    },
+    beforeDestroy() {
+
+    },
+  };
+</script>
+
+<style sccoped>
+
+</style>
