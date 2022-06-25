d4163a343951543e1d82bc924a37da84e19cabd2
Sungjae.Kwak <sungjae8970@gmail.com>
2022-06-26 02:28:37 +0900

    .vue 파일

---
+++ 8.Mineswipper/MineForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="mine-form">
+    <h2>This is mine form, it is related to making mine board</h2>
+    <input type="number" placeholder="세로" :value="row" @change="onChangeRow" />
+    <input type="number" placeholder="가로" :value="col" @change="onChangeCol" />
+    <input type="number" placeholder="지뢰" :value="mine" @change="onChangeMine" />
+    <button @click="onClickBtn">게임 시작</button>
+  </div>
+</template>
+
+<script>
+  import { START_GAME } from './store';
+
+  export default {
+    data() {
+      return {
+        row: 10,
+        col: 10,
+        mine: 10
+      };
+    },
+    methods: {
+      onChangeRow(e) {
+        this.row = e.target.value;
+      },
+      onChangeCol(e) {
+        this.col = e.target.value;
+      },
+      onChangeMine(e) {
+        this.mine = e.target.value;
+      },
+      onClickBtn() {
+        console.log(this.row, this.col, this.mine);
+        this.$store.commit(START_GAME, { row: this.row, col: this.col, mine: this.mine });
+      }
+    }
+  }
+</script>
+
+<style>
+  .mine-form {
+    border: 1px solid black;
+  }
+</style>
---
+++ 8.Mineswipper/TableComponent.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="table-component">
+    <h2>This is table-component</h2>
+    <table>
+      <tr v-for="(rowData, rowIndex) in tableData" :key="rowIndex">
+        <td 
+          v-for="(cellData, colIndex) in rowData"
+          :key="colIndex" 
+          :style="cellDataStyle(rowIndex, colIndex)"
+          @click="onClickTd(rowIndex, colIndex)"
+          @contextmenu.prevent="onRightClickTd(rowIndex, colIndex)"
+        >
+          {{cellDataText(rowIndex, colIndex)}}
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+  import { mapState } from 'vuex';
+  import { CLICK_MINE, CODE, FLAG_CELL, NORMALIZE_CELL, OPEN_CELL, QUESTION_CELL } from './store';
+
+  export default {
+        computed: {
+      ...mapState(['tableData', 'halted']),
+      cellDataStyle(state) {
+        return (row, col) => {
+          switch (this.$store.state.tableData[row][col]) {
+            case CODE.NORMAL:
+            case CODE.MINE:
+              return {
+                background: '#444',
+              };
+            case CODE.CLICKED_MINE:
+            case CODE.OPENED:
+              return {
+                background: 'white',
+              };
+            case CODE.FLAG:
+            case CODE.FLAG_MINE:
+              return {
+                background: 'red',
+              };
+            case CODE.QUESTION:
+            case CODE.QUESTION_MINE:
+              return {
+                background: 'yellow',
+              };
+            default:
+              return {};
+          }
+        };
+      },
+      cellDataText() {
+        return (row, col) => {
+          switch (this.$store.state.tableData[row][col]) {
+            case CODE.MINE:
+              return 'X';
+            case CODE.NORMAL:
+              return '';
+            case CODE.FLAG_MINE:
+            case CODE.FLAG:
+              return '!';
+            case CODE.QUESTION_MINE:
+            case CODE.QUESTION:
+              return '?';
+            case CODE.CLICKED_MINE:
+              return '펑';
+            default:
+              return this.$store.state.tableData[row][col] || '';
+          }
+        };
+      },
+    },
+    methods: {
+      onClickTd(row, col) {
+        if(this.halted) {
+          return;
+        }
+        this.$store.commit(OPEN_CELL, { row, col });
+      },
+      onRightClickTd(row, col) {
+        if (this.halted) {
+          return;
+        }
+        switch (this.tableData[row][col]) {
+          case CODE.NORMAL:
+          case CODE.MINE:
+            this.$store.commit(FLAG_CELL, { row, col });
+            return;
+          case CODE.FLAG:
+          case CODE.FLAG_MINE:
+            this.$store.commit(QUESTION_CELL, { row, col });
+            return;
+          case CODE.QUESTION:
+          case CODE.QUESTION_MINE:
+            this.$store.commit(NORMALIZE_CELL, { row, col });
+            return;
+          default:
+            return;
+        }
+      },
+    },
+  };
+</script>
+
+<style>
+  .table-component {
+    border: 1px solid black;
+  }
+</style>
